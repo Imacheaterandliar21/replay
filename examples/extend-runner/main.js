9fee918d611d060f7f0846f3defe2bd5ab4dfff3
Imacheaterandliar21 <87601277+Imacheaterandliar21@users.noreply.github.com>
2023-07-18 11:28:28 -0400

    Delete please
    I don't want this on my mobile phone no more

--- examples/extend-runner/main.js
+++ examples/extend-runner/main.js
@@ -1,9 +1,9 @@
 import { createRunner, PuppeteerRunnerExtension } from '../../lib/main.js';
-import puppeteer from 'puppeteer';
+import puppeteer from 'puppeteer';delete 
 
 const browser = await puppeteer.launch({
   headless: 'new',
-});
+});delete 
 
 const page = await browser.newPage();
 
@@ -11,23 +11,23 @@ class Extension extends PuppeteerRunnerExtension {
   async beforeAllSteps(flow) {
     await super.beforeAllSteps(flow);
     console.log('starting');
-  }
+  }delete 
 
   async beforeEachStep(step, flow) {
     await super.beforeEachStep(step, flow);
     console.log('before', step);
-  }
+  }delete 
 
   async afterEachStep(step, flow) {
     await super.afterEachStep(step, flow);
     console.log('after', step);
-  }
+  }delete 
 
   async afterAllSteps(flow) {
     await super.afterAllSteps(flow);
     console.log('done');
-  }
-}
+  }delete 
+}delete 
 
 const runner = await createRunner(
   {
@@ -36,12 +36,14 @@ const runner = await createRunner(
       {
         type: 'navigate',
         url: 'https://wikipedia.org',
-      },
+      },delete 
     ],
   },
   new Extension(browser, page, 7000)
-);
+);delete 
 
 await runner.run();
 
 await browser.close();
+delete 
+  
